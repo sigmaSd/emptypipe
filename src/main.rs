157d40c726407ac2b9a1c718491e2363f3e0f726
Nbiba Bedis <bedisnbiba@gmail.com>
2019-04-03 16:34:06 +0100

    emptypipe

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::io::{self, Read};
+use std::env;
+use std::process::Command;
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer)?;
+
+    if buffer.trim().is_empty() {
+		Command::new(env::args().nth(1).expect("No command given"))
+			.args(&env::args().skip(2).collect::<Vec<String>>())
+			.spawn()?;    	
+    }
+    Ok(())
+}
