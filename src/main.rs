7f7eeff0f2d8c6ca8b58e5b0785169af98940f27
Nbiba Bedis <bedisnbiba@gmail.com>
2019-04-03 18:41:29 +0100

    else case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{self, Read};
 use std::env;
+use std::io::{self, Read};
 use std::process::Command;
 
 fn main() -> io::Result<()> {
@@ -10,9 +10,11 @@ fn main() -> io::Result<()> {
     handle.read_to_string(&mut buffer)?;
 
     if buffer.trim().is_empty() {
-		Command::new(env::args().nth(1).expect("No command given"))
-			.args(&env::args().skip(2).collect::<Vec<String>>())
-			.spawn()?;    	
+        Command::new(env::args().nth(1).expect("No command given"))
+            .args(&env::args().skip(2).collect::<Vec<String>>())
+            .spawn()?;
+    } else {
+        print!("{}", &buffer);
     }
     Ok(())
 }
