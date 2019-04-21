5d0c0301edfbbce2abb3c0d758d305a0ae7d507e
Nbiba Bedis <bedisnbiba@gmail.com>
2019-04-21 19:03:51 +0100

    not empty

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,24 @@ use std::io::{self, Read};
 use std::process::Command;
 
 fn main() -> io::Result<()> {
+    let inv = match env::args().nth(1) {
+        Some(ref flag) if flag == "-i" => true,
+        _ => false,
+    };
+
     let mut buffer = String::new();
     let stdin = io::stdin();
     let mut handle = stdin.lock();
 
     handle.read_to_string(&mut buffer)?;
 
-    if buffer.trim().is_empty() {
+    if buffer.trim().is_empty() && !inv {
         Command::new(env::args().nth(1).expect("No command given"))
             .args(&env::args().skip(2).collect::<Vec<String>>())
+            .spawn()?;
+    } else if !buffer.trim().is_empty() && inv {
+        Command::new(env::args().nth(2).expect("No command given"))
+            .args(&env::args().skip(3).collect::<Vec<String>>())
             .spawn()?;
     } else {
         print!("{}", &buffer);
